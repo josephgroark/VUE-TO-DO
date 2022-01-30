ada4e4daeb98f6660ffe7462e3b3989c822921b3
Joey <josephgroark@gmail.com>
2022-01-30 17:10:19 +0000

    initial commit

---
+++ src/components/Button.vue
@@ -0,0 +1,18 @@
+<template>
+    <button @click="addTask()" :style={background:color} class="btn">{{text}}</button>
+</template>
+
+<script>
+export default {
+    name: "Button",
+    props: {
+        text: String,
+        color: String
+    },
+    methods: {
+        addTask() {
+            this.$emit('btn-click')
+        }
+    }
+}
+</script>
---
+++ src/components/Footer.vue
@@ -0,0 +1,27 @@
+<template>
+    <footer>
+        <p>Copyright &copy; {{year}}</p>
+        <router-link to="/about">About</router-link>
+    </footer>
+</template>
+
+<script>
+export default {
+    name: 'Footer',
+    props: {
+        year: Number
+    }
+}
+</script>
+
+
+<style scoped>
+a {
+    color: #333
+}
+
+footer {
+    margin: top 50px;
+    text-align: center
+}
+</style>
